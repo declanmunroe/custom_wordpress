61c07ec1231a7b1d7baa260f7420b3cc2e652e33
Declan Munroe <decofeco29@gmail.com>
2018-08-24 16:28:35 +0100

    Testing loop

--- wp-content/themes/declan/index.php
+++ wp-content/themes/declan/index.php
@@ -2,6 +2,15 @@
 
 <section class="container">
     <h4>This is the body content section</h4>
+    <div class="row">
+        <?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="col-md-3">
+            
+                <?php the_content(); ?>
+        </div>
+        <?php endwhile; endif; ?>
+    </div>
 </section>
 
 <?php get_footer(); ?>
